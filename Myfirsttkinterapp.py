98efdb313531093fd4e33dee72084dc2cae6853a
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-06-02 04:11:42 -0700

    simple gui

---
+++ Myfirsttkinterapp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 12:33:44 2017
+
+@author: Ahmad khan
+"""
+import tkinter
+root = tkinter.Tk()
+
+root.title("My First Python App using tkinter")
+
+root.geometry("500x500+400+100")
+root.mainloop()   
+           
