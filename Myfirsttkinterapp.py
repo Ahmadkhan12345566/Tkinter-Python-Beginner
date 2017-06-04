5531110dc2ac41f6c25c15a4192736828f900e72
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-06-04 12:46:48 -0700

    testing connecting database

--- Myfirsttkinterapp.py
+++ Myfirsttkinterapp.py
@@ -5,7 +5,31 @@ Created on Tue May 30 12:33:44 2017
 @author: Ahmad khan
 """
 import tkinter
+import mysql.connector
 root = tkinter.Tk()
+cnx = mysql.connector.connect(user='obe', database='obe',password='obe')
+cursor = cnx.cursor()
+l2="kk2"
+#function
+def hello(event):
+
+    add_employee = ("INSERT INTO abc "
+               "(name, anting) "
+               "VALUES ('1', '1')")
+    cursor.execute(add_employee)
+    cnx.commit()
+
+    cursor.close() 
+    cnx.close()
+
+
+l1 = tkinter.Label(text=l2, fg="black", bg="white").pack()
+t1 = tkinter.Text( fg="black", bg="white",).pack()
+b1 = tkinter.Button(text="Test", fg="black", bg="white",width="50")
+
+#buindingbutton
+b1.bind('<Button-1>', hello)
+b1.pack()
 
 root.title("My First Python App using tkinter")
 
